54fc238d79ddd3dbb5cdd20d06ac7c8482cb647f
Dhanashri <dhanashri@ratibpal.com>
2022-08-17 02:18:04 +0530

    RecyclerView call in main

--- app/src/main/java/com/assignmentloudlytetrislist/MainActivity.java
+++ app/src/main/java/com/assignmentloudlytetrislist/MainActivity.java
@@ -1,14 +1,23 @@
 package com.assignmentloudlytetrislist;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        recyclerView = findViewById(R.id.recyclerView);
+        RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getApplicationContext());
+        recyclerView.setLayoutManager(mLayoutManager);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
     }
 }
