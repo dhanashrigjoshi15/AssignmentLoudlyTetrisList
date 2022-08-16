a2e35181879ee859fc404f279f55e6ecd1c31293
Dhanashri <dhanashri@ratibpal.com>
2022-08-17 04:38:38 +0530

    Minor UI and logic changes

--- app/src/main/java/com/assignmentloudlytetrislist/CustomAdapter.java
+++ app/src/main/java/com/assignmentloudlytetrislist/CustomAdapter.java
@@ -1,16 +1,30 @@
+//Adapter to set data of recyclerview as a list
+
 package com.assignmentloudlytetrislist;
 
+import android.content.Context;
+import android.graphics.Color;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.CustomViewHolder> {
-    public CustomAdapter(){
+import com.assignmentloudlytetrislist.data.RepositoryData;
 
+import java.util.ArrayList;
+
+public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.CustomViewHolder> {
+    private ArrayList<RepositoryData> repositoryDataList;
+    private Context context;
+
+    //constructor
+    public CustomAdapter(Context context, ArrayList<RepositoryData> repositoryDataList ){
+        this.context = context;
+        this.repositoryDataList = repositoryDataList;
     }
 
     @NonNull
@@ -23,24 +37,34 @@ public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.CustomView
 
     @Override
     public void onBindViewHolder(@NonNull CustomAdapter.CustomViewHolder holder, int position) {
-
+        long id = repositoryDataList.get(position).getId();
+        holder.textViewName.setText(repositoryDataList.get(position).getRepoName());
+        holder.textViewLoginName.setText(repositoryDataList.get(position).getOwner().getLoginName());
+        holder.textViewSize.setText(repositoryDataList.get(position).getRepoSize()+"");
+        //For has_wiki as true, display item with Gray background
+        if(repositoryDataList.get(position).isHasWiki())
+            holder.cardViewItem.setCardBackgroundColor(Color.LTGRAY);
+        else
+            holder.cardViewItem.setCardBackgroundColor(Color.WHITE);
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return repositoryDataList.size();
     }
 
     public class CustomViewHolder extends RecyclerView.ViewHolder{
         private TextView textViewName;
         private TextView textViewLoginName;
         private TextView textViewSize;
+        private CardView cardViewItem;
 
         public CustomViewHolder(@NonNull View itemView) {
             super(itemView);
             textViewName = itemView.findViewById(R.id.textViewName);
             textViewLoginName = itemView.findViewById(R.id.textViewLoginName);
             textViewSize = itemView.findViewById(R.id.textViewSize);
+            cardViewItem = itemView.findViewById(R.id.main_container);
         }
     }
 }
--- app/src/main/java/com/assignmentloudlytetrislist/MainActivity.java
+++ app/src/main/java/com/assignmentloudlytetrislist/MainActivity.java
@@ -2,14 +2,29 @@ package com.assignmentloudlytetrislist;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.DefaultItemAnimator;
-import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
 
+import com.assignmentloudlytetrislist.data.APIResponse;
+import com.assignmentloudlytetrislist.data.RepositoryData;
+import com.assignmentloudlytetrislist.retrofit.RetrofitClient;
+import com.assignmentloudlytetrislist.retrofit.RetrofitInterface;
+
+import java.util.ArrayList;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+import static com.assignmentloudlytetrislist.Constants.GET_VALUE;
+
 public class MainActivity extends AppCompatActivity {
-    RecyclerView recyclerView;
+    private RecyclerView recyclerView;
+    private ArrayList<RepositoryData> repositoryDataList;
+    private CustomAdapter customAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,5 +34,38 @@ public class MainActivity extends AppCompatActivity {
         RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getApplicationContext());
         recyclerView.setLayoutManager(mLayoutManager);
         recyclerView.setItemAnimator(new DefaultItemAnimator());
+        getRepositoryDataListFromRestApi();
+    }
+
+    //Get repository data from API
+    private void getRepositoryDataListFromRestApi() {
+
+        final ProgressDialog progressDialog = new ProgressDialog(MainActivity.this);
+        progressDialog.setCancelable(false); // set cancelable to false
+        progressDialog.setMessage("Please Wait"); // set message
+        progressDialog.show();
+
+        //initialize retrofit
+        RetrofitInterface retrofitInterface = RetrofitClient.getClient().create(RetrofitInterface.class);
+
+        //retrofit get call with GET_VALUE as tetris
+        Call<APIResponse> call = retrofitInterface.getRepositories(GET_VALUE);
+        call.enqueue(new Callback<APIResponse>() {
+            @Override
+            public void onResponse(Call<APIResponse> call, Response<APIResponse> response) {
+
+                progressDialog.dismiss();
+                APIResponse apiResponse = response.body();
+                repositoryDataList = apiResponse.getRepositoryDataList();
+                customAdapter = new CustomAdapter(getApplicationContext(), repositoryDataList);
+                recyclerView.setAdapter(customAdapter);
+
+            }
+
+            @Override
+            public void onFailure(Call<APIResponse> call, Throwable t) {
+                progressDialog.dismiss();
+            }
+        });
     }
 }
